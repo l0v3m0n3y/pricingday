915eefa98bb2b82449ba08731d57ac7738ce6d42
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:24:47 +0300

    Add HTTPMethod enum to pricingday.swift

--- src/pricingday.swift
+++ src/pricingday.swift
@@ -20,6 +20,13 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
 
 public class Pricingday {
     private let api = "https://pricing.day/_next/data"
